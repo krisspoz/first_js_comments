92f7a7b3bf8a9bfef5e4d3c7adc4142d572b312c
Krzysztof Stankowski <Krzysztof Stankowski>
2017-01-26 19:18:34 +0100

    js comments

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// plik scripts.js
+
+var name = prompt('Wpisz swoje imię');
+// my first js comment
+alert('Witaj, ' + name);
+// my second js comment
+console.log('Witaj, ' + name);
+/* my third js comment
+block comment */
